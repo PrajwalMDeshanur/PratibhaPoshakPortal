b20aae2e5c37c716d06f796271a258c38b8c614e
Prajwal56 <prajwalmd56@gmail.com>
2025-06-10 07:59:57 +0530

    Revert "updated shortlist process2"
    This reverts commit a74064e23cc4ddc001b6a5eb9cbe2f0adf3d0f71.

--- client/src/pages/Shortlisting.js
+++ client/src/pages/Shortlisting.js
@@ -8,6 +8,7 @@ const Shortlisting = () => {
       <h2 className="text-center">Shortlisting</h2>
 
       <div className="shortlisting-steps mt-4">
+       
         <div className="option-box">
           <Link to="/generate-shortlist" className="option-link">
             <div className="icon-box">
@@ -17,10 +18,11 @@ const Shortlisting = () => {
           </Link>
         </div>
 
+        
         <div className="option-box">
           <Link to="/shortlist-info" className="option-link">
             <div className="icon-box">
-              <i className="fas fa-info-circle"></i> {/* Changed icon for clarity */}
+              <i className="fas fa-plus-circle"></i>
             </div>
             <div className="text-box">Shortlisted Information</div>
           </Link>
@@ -31,22 +33,23 @@ const Shortlisting = () => {
         <h1>Shortlisting Process</h1>
         <h3>Instructions</h3>
         <ul>
-          <li>
+        <li>
             The first step is to generate a shortlist based on the state,
-            district, and optionally multiple blocks, using specific criteria.
+            district, and optionally multiple blocks.
           </li>
           <li>
-            You can generate a shortlist multiple times.
-            Only one shortlist can exist for a specific block at any given time.
-            To finalize and secure a shortlist, you need to freeze it from the
+            You can generate a shortlist multiple times. However, if you want to
+            finalize and lock the shortlist, you need to freeze it from the
             "Shortlisted Information" interface.
-            Similarly, deletion of a shortlist is also done through the "Shortlisted Information" interface.
           </li>
           <li>
             From the "Shortlisted Information" interface, you can access all the
-            details of the generated shortlist and can download the selected student list from there.
+            details of the shortlists.
           </li>
-          
+          <li>
+            You also have the options to delete the shortlist by name and
+            download the list of shortlisted students.
+          </li>
         </ul>
       </div>
     </div>
